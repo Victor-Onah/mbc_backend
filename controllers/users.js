da49e9bcfc5d5f6340b860f6987a4d4b738efb30
Victor <victoronah.dev@gmail.com>
2023-07-23 17:24:45 +0100

    updated the user.js file in the controller folder

--- controllers/users.js
+++ controllers/users.js
@@ -23,20 +23,38 @@ const sendMessage = async (req, res, next) => {
 
 // Send message
 const placeOrder = async (req, res, next) => {
-	try {
-		let { order, phoneNumber, name } = req.body;
-		await Order.create({
-			phoneNumber,
-			name,
-			order,
+	await import('formidable')
+		.then((module) => {
+			let form = new module.IncomingForm({
+				keepExtensions: true,
+			});
+			form.parse(req, async (error, fields, files) => {
+				if (error) {
+					return res.json({
+						success: false,
+						message: 'An unexpected error occurred',
+					});
+				}
+				try {
+					let { name, phoneNumber, order } = fields;
+					await Order.create({
+						name: name[0],
+						phoneNumber: phoneNumber[0],
+						order: order[0],
+					});
+					res.json({ success: true });
+				} catch (error) {
+					res.json({ success: false, message: 'An unexpected error occurred' });
+					next(error);
+				}
+			});
+		})
+		.catch((error) => {
+			res.json({
+				success: false,
+				message: 'An unexpected error occurred',
+			});
+			next(error);
 		});
-		res.json({ success: true });
-	} catch (error) {
-		res.json({
-			success: false,
-			message: 'An unexpected error occurred',
-		});
-		next(error);
-	}
 };
 module.exports = { sendMessage, placeOrder };
